5f024da038716abed313e7f06d7419f6d458a92e
Tomas Havetta <havetta@trigan.cz>
2023-02-05 13:00:16 +0100

    Doplneni poznamek

--- Singleton/JenJeden/Program.cs
+++ Singleton/JenJeden/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace JenJeden
+﻿namespace JenJeden;
+
+/// <summary>
+/// Program samotný nic neřeší.
+/// Třídy Singleton1 až 6 jsou ukázkou několika cest, jak zajistit v .NETu realizaci Singletonu
+///
+/// Podle knížky C# in Depth, autor Jon Skeet
+/// </summary>
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
